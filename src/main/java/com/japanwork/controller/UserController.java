8dbfa44810132d8fea459e6378a99d403e874750
PhuNguyen240196 <38210727+PhuNguyen240196@users.noreply.github.com>
2019-08-28 19:34:53 +0700

    feature/get-profile update model user

--- src/main/java/com/japanwork/controller/UserController.java
+++ src/main/java/com/japanwork/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.japanwork.controller;
 
+import java.util.Set;
 import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,9 +9,9 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.japanwork.constant.CommonConstant;
@@ -55,12 +56,11 @@ public class UserController {
     }
 
     @GetMapping(value = UrlConstant.URL_PROFILE)
-    public ResponseDataAPI getProfile(@PathVariable UUID id) {
-    	User user = commonSupport.loadUserById(id);
+    public ResponseDataAPI getProfile(@RequestParam(name = "ids") Set<UUID> ids) {
     
     	return new ResponseDataAPI(
     	        CommonConstant.ResponseDataAPIStatus.SUCCESS,
-    	        userService.getProfile(user),
+    	        userService.getProfile(ids),
                 ""
         );
     }
